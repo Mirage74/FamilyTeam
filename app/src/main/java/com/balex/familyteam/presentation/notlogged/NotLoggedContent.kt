c75525f1fc9711a1d59433a3373a5b6966714857
balex <balexvicx@gmail.com>
2024-06-24 20:52:15 +0200

    lang

--- app/src/main/java/com/balex/familyteam/presentation/notlogged/NotLoggedContent.kt
+++ app/src/main/java/com/balex/familyteam/presentation/notlogged/NotLoggedContent.kt
@@ -1,7 +1,12 @@
 package com.balex.familyteam.presentation.notlogged
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +15,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Language
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +31,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,50 +59,138 @@ fun NotLoggedContent(component: NotLoggedComponent) {
         color = colorResource(R.color.orange_gold)
     ) {
 
-//        Column(
-//            modifier = Modifier.fillMaxSize().padding(16.dp),
-//            horizontalAlignment = Alignment.CenterHorizontally,
-//            verticalArrangement = Arrangement.Center
-//        ) {
-//            Row(
-//                verticalAlignment = Alignment.CenterVertically
-//            ) {
-//                IconButton(onClick = { /* Действие по переключению языка */ },
-//                    modifier = Modifier.size(48.dp) ) {
-//                    Icon(
-//                        painter = painterResource(id = R.drawable.ic_language), // Замените на ваш ресурс иконки
-//                        contentDescription = "Переключить язык"
-//                    )
-//                }
-//                Spacer(modifier = Modifier.width(16.dp)) // Пробел между значком и кнопкой
-//                Button(
-//                    onClick = { /* Действие кнопки */ },
-//                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF1E88E5)) // Синий цвет кнопки
-//                ) {
-//                    Text(
-//                        text = "Синяя кнопка",
-//                        maxLines = 1, // Ограничение одной строкой
-//                        overflow = TextOverflow.Ellipsis // Текст с многоточием, если не помещается
-//                    )
-//                }
-//            }
-
-
-            when (state.logChooseState) {
-                NotLoggedStore.State.LogChooseState.Initial -> {
-                    ThreeButtonsScreen(false)
-                }
-
-                NotLoggedStore.State.LogChooseState.ErrorLoadingUserData -> {
-                    TODO()
-                }
-
-                NotLoggedStore.State.LogChooseState.NoSavedUserFound -> {
-                    ThreeButtonsScreen(true)
-                }
+        when (state.logChooseState) {
+            NotLoggedStore.State.LogChooseState.Initial -> {
+                LanguageChooserScreen(false)
             }
+
+            NotLoggedStore.State.LogChooseState.ErrorLoadingUserData -> {
+                TODO()
+            }
+
+            NotLoggedStore.State.LogChooseState.NoSavedUserFound -> {
+                LanguageChooserScreen(true)
+            }
+        }
         //}
     }
+}
+
+@Composable
+fun LanguageChooserScreen(isEbabled: Boolean) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        SwitchLanguage()
+        ThreeButtonsScreen(isEbabled = isEbabled)
+    }
+}
+
+
+@Composable
+fun SwitchLanguage() {
+    var isChooseOptionDropMenuExpanded by rememberSaveable { mutableStateOf(false) }
+    val languages = listOf("English", "Spanish", "French")
+
+    Box(
+        modifier = Modifier
+            .padding(16.dp, 0.dp, 0.dp, 0.dp)
+//            .background(Color.White)
+//            .border(width = 1.dp, color = Color.Black),
+    ) {
+
+        Row(
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+
+
+            Row(
+                modifier = Modifier
+                    .height(48.dp)
+                    .padding(4.dp)
+                    .fillMaxWidth()
+                    .wrapContentWidth(align = Alignment.End)
+                    .background(Color.White)
+                    .border(width = 1.dp, color = Color.Black),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+
+            ) {
+
+
+            IconButton(
+                onClick = { /* Действие по переключению языка */ },
+                modifier = Modifier.size(48.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_language), // Замените на ваш ресурс иконки
+                    contentDescription = "Переключить язык"
+                )
+            }
+                Spacer(modifier = Modifier.width(4.dp)) // Пробел между значком и кнопкой
+            Text(
+                modifier = Modifier.clickable {
+                    isChooseOptionDropMenuExpanded = !isChooseOptionDropMenuExpanded
+                },
+                text = "EN"
+            )
+            //Spacer(modifier = Modifier.width(4.dp))
+            IconButton(onClick = {
+                isChooseOptionDropMenuExpanded = !isChooseOptionDropMenuExpanded
+            }) {
+                Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = null)
+            }
+        }
+
+        DropdownMenu(
+            //expanded = terminalState.value.isChooseOptionDropMenuExpanded,
+            expanded = isChooseOptionDropMenuExpanded,
+            onDismissRequest = {
+                isChooseOptionDropMenuExpanded = false
+            },
+
+            modifier = Modifier
+                .width(150.dp)
+                .background(Color.White)
+                .padding(0.dp)
+                .border(1.dp, Color.Black)
+
+        ) {
+            languages.forEach { option ->
+                DropdownMenuItem(
+                    modifier = Modifier
+                        .padding(0.dp)
+                        .border(1.dp, Color.LightGray),
+                    onClick = {
+                        val selectedLanguage = when (option) {
+                            "English" -> "en"
+                            "Spanish" -> "es"
+                            "French" -> "fr"
+                            else -> "en"
+                        }
+                        isChooseOptionDropMenuExpanded = false
+
+//                        onDropDownMenuStateChanged(
+//                            dropDownMenuState.value.copy(
+//                                selectedOption = option,
+//                                selectedAsset = selectedLanguage
+//                            )
+//                        )
+                    },
+                    text = {
+                        Text(
+                            text = option
+                            //,
+                            //fontSize = LIST_OPTIONS_TEXT_SIZE
+                        )
+                    },
+                )
+            }
+        }
+    }
+}
 }
 
 @Composable
@@ -98,13 +201,13 @@ fun ThreeButtonsScreen(isEbabled: Boolean) {
             .padding(horizontal = 64.dp),
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        IconButton(onClick = { /* Действие по переключению языка */ },
-            modifier = Modifier.size(148.dp) ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_language), // Замените на ваш ресурс иконки
-                contentDescription = "Switch language"
-            )
-        }
+//        IconButton(onClick = { /* Действие по переключению языка */ },
+//            modifier = Modifier.size(148.dp) ) {
+//            Icon(
+//                painter = painterResource(id = R.drawable.ic_language), // Замените на ваш ресурс иконки
+//                contentDescription = "Switch language"
+//            )
+//        }
         Button(
             onClick = { /* TODO: Handle button click */ },
             modifier = Modifier
